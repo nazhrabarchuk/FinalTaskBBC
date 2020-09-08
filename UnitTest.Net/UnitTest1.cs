ec26b39d3d254ee5d0a83385e2f8acdb36d35e8d
nazhr <nazhrabarchuk@gmail.com>
2020-09-08 20:14:07 +0300

    Добавьте файлы проекта.

---
+++ UnitTest.Net/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTest.Net
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();  // Creates a new Chrome instance and opens the browser
+            driver.Navigate().GoToUrl("https://www.bbc.com");  // Navigates to a page by address
+
+        }
+    }
+}
